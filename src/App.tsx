46af3748cd071c859f5de2a121edd69774ef482b
Erwin G <erwingaitano@gmail.com>
2022-06-09 00:30:28 -0500

    added jump in place button

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import './App.css';
 
 const LOCALSTORAGE_KEY = 'goerwin_gymNeat';
 
-function getLSItem(name: string) {
+function getLSDateData(date: string) {
   try {
-    const data: any = JSON.parse(window.localStorage.getItem(LOCALSTORAGE_KEY)!);
+    const data: any = JSON.parse(
+      window.localStorage.getItem(LOCALSTORAGE_KEY)!
+    );
 
-    return data[name] as string;
+    return data[date] as any;
   } catch (e) {}
 
   return null;
 }
 
-function setLSItem(name: string, value: string) {
+function setLSDateData(name: string, value: any) {
   try {
     let data: any = JSON.parse(window.localStorage.getItem(LOCALSTORAGE_KEY)!);
     if (!data) data = {};
@@ -30,20 +32,49 @@ function setLSItem(name: string, value: string) {
 export default function App() {
   const today = new Date();
   const todayISO = today.toISOString().replace(/T.*/, '');
-  const jipTodayKey = `${todayISO}_jumpInPlace`;
-  const jipTodayData = Number(getLSItem(jipTodayKey));
-  const [jipCount, setJipCount] = useState(jipTodayData);
+  const todayData = getLSDateData(todayISO) || {};
+  const [jipData, setJipData] = useState((todayData.jip || []) as any[]);
+  const [openJipData, setOpenJipData] = useState(false);
 
   useEffect(() => {
-    setLSItem(jipTodayKey, String(jipCount));
-  }, [jipCount]);
+    const todayData = getLSDateData(todayISO) || {};
+    setLSDateData(todayISO, { ...todayData, jip: jipData });
+  }, [jipData]);
 
   return (
     <div className="app-container">
       <p className="app-date">{today.toDateString()}</p>
-      <button className="app-button" onClick={() => setJipCount(jipCount + 1)}>
+      <button
+        className="app-button"
+        onClick={() => setJipData([...jipData, new Date().toISOString()])}
+      >
         JUMP IN PLACE
-        <p>Counter: {jipCount}</p>
+        <p>Counter: {jipData.length}</p>
+      </button>
+
+      <button
+        className="app-greenbutton"
+        onClick={() => setOpenJipData(!openJipData)}
+      >
+        VIEW JUMP IN PLACE DATA
+      </button>
+
+      <dialog open={openJipData}>
+        <h3>JIP Data</h3>
+        {jipData.map((el, idx) => (
+          <p key={idx}>{new Date(el).toLocaleTimeString()}</p>
+        ))}
+        <button onClick={() => setOpenJipData(false)}>Close</button>
+      </dialog>
+
+      <button
+        className="app-redbutton"
+        onClick={() => {
+          const ok = window.confirm('Reset Jump In Place?');
+          if (ok) setJipData([]);
+        }}
+      >
+        RESET JUMP IN PLACE
       </button>
     </div>
   );
