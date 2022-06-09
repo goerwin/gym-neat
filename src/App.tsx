1543ee8b2602b88527a183d68e9c95d2ec9a3fc5
Erwin G <erwingaitano@gmail.com>
2022-06-08 23:46:08 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
-function App() {
+const LOCALSTORAGE_KEY = 'goerwin_gymNeat';
+
+function getLSItem(name: string) {
+  try {
+    const data: any = JSON.parse(window.localStorage.getItem(LOCALSTORAGE_KEY)!);
+
+    return data[name] as string;
+  } catch (e) {}
+
+  return null;
+}
+
+function setLSItem(name: string, value: string) {
+  try {
+    let data: any = JSON.parse(window.localStorage.getItem(LOCALSTORAGE_KEY)!);
+    if (!data) data = {};
+
+    data[name] = value;
+    window.localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+  } catch (e) {
+    return false;
+  }
+
+  return true;
+}
+
+export default function App() {
+  const today = new Date();
+  const todayISO = today.toISOString().replace(/T.*/, '');
+  const jipTodayKey = `${todayISO}_jumpInPlace`;
+  const jipTodayData = Number(getLSItem(jipTodayKey));
+  const [jipCount, setJipCount] = useState(jipTodayData);
+
+  useEffect(() => {
+    setLSItem(jipTodayKey, String(jipCount));
+  }, [jipCount]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <p className="app-date">{today.toDateString()}</p>
+      <button className="app-button" onClick={() => setJipCount(jipCount + 1)}>
+        JUMP IN PLACE
+        <p>Counter: {jipCount}</p>
+      </button>
     </div>
   );
 }
-
-export default App;
